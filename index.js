f1d922e51644cb3077be44235c4b65872fd9126c
amitdasgupta <amitdsgupta007@gmail.com>
2020-04-09 20:13:14 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 const db = require("./config/mongoose");
 const path = require("path");
