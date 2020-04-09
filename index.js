44eb95e52db9abb9236bba374d361c7847de5497
amitdasgupta <amitdsgupta007@gmail.com>
2020-04-09 17:52:33 +0530

    updating port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port = 8000;
+const port = process.env.port || 8000;
 const app = express();
 const db = require("./config/mongoose");
 
